9759f4541dc7f204620872b51534214fb6174637
asinghamgoodwin <asinghamgoodwin@gmail.com>
2015-08-31 18:00:40 -0400

    trying out connecting our python code with html

--- app/views.py
+++ app/views.py
@@ -1,14 +1,18 @@
 from flask import render_template
 from app import app
+import connectFactour
 
 @app.route('/')
 @app.route('/index')
 @app.route('/kitties')
 def index():
-    gamer = {"letter":"O"}
+    #gamer = {"letter":"O"}
+    gamer1 = gamer('X')
+    gamer2 = gamer('O')
+    newGame = game(gamer1, gamer2)
     return render_template('index.html',
                             title='Connect Factour!',
-                            player = gamer)
+                            game=newGame)
 
 
 
