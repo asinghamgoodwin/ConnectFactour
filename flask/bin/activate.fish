44e573b265547a3fd9c6a1c8cb26c13e3fb3ef74
asinghamgoodwin <asinghamgoodwin@gmail.com>
2015-09-08 11:15:39 -0400

    ignoring my virtual environment

--- flask/bin/activate.fish
+++ flask/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/shad/codespace/factic-toe/flask"
+set -gx VIRTUAL_ENV "/Users/atsg/Documents/Coding/RCprojects/ConnectFactour/VENVflask"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
