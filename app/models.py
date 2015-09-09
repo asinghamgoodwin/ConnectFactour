11d08bb0d4e9db0a70803169b972d992c955ba67
asinghamgoodwin <asinghamgoodwin@gmail.com>
2015-09-09 16:52:52 -0400

    making radio buttons and a database

---
+++ app/models.py
@@ -0,0 +1,9 @@
+from app import db
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    gameURL = db.Column(db.String(64), index=True, unique=True)
+    gameState = db.Column(db.String(64), index=True, unique=True)
+
+    def __repr__(self):
+        return '<Game %r>' % (self.gameURL)
--- app/views.py
+++ app/views.py
@@ -1,18 +1,21 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from connectFactour import *
+from .forms import coinMoveForm
 
 @app.route('/')
 @app.route('/index')
 @app.route('/kitties')
 def index():
     #gamer = {"letter":"O"}
+    form = coinMoveForm()
     gamer1 = gamer('X')
     gamer2 = gamer('O')
     newGame = game(gamer1, gamer2)
     return render_template('index.html',
                             title='Connect Factour!',
-                            game=newGame)
+                            game=newGame,
+                            form=form)
 
 
 
