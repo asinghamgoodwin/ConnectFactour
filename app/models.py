965c6bff19b2ea2ff07deb1322c8cb341d8b0015
Shad Hopson <hopson.shad@gmail.com>
2015-09-21 19:21:07 -0400

    made starting web page

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from app import db
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     gameURL = db.Column(db.String(64), index=True, unique=True)
-    gameState = db.Column(db.String(64), index=True, unique=True)
+    gameState = db.Column(db.String(64), index=True, unique=False)
 
     def __repr__(self):
         return '<Game %r>' % (self.gameURL)
